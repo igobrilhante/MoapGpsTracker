2f1a2012f59e75bd835f26608f99021565d6004b
igobrilhante <brilhanteigo@gmail.com>
2013-01-03 00:20:29 -0300

    a

--- src/org/moap/gpstracker/oauth/FoursquareCredentials.java
+++ src/org/moap/gpstracker/oauth/FoursquareCredentials.java
@@ -2,9 +2,9 @@ package org.moap.gpstracker.oauth;
 
 public class FoursquareCredentials {
 
-	public static final String CLIENT_ID = "S5PZNYCU03EC3O2UOK2PJJECCOJPYVCPFWFWNCIJ0AAWQ13Z";
-	public static final String CLIENT_SECRET = "12T3ZMVFGK22A4MWCEFN1GNA4HDRGFALUX1XPQVUECKBIJYV";
-	public static final String CLIENT_CALLBACK = "moapgpstracker://connect";
+	public static final String CLIENT_ID = "";
+	public static final String CLIENT_SECRET = "";
+	public static final String CLIENT_CALLBACK = "";
 	
 
 }
