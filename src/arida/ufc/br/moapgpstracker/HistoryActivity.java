39d4e00620bd9936641827c8093dddd79ffbb059
igobrilhante <brilhanteigo@gmail.com>
2013-01-03 00:17:51 -0300

    OK!

--- src/arida/ufc/br/moapgpstracker/HistoryActivity.java
+++ src/arida/ufc/br/moapgpstracker/HistoryActivity.java
@@ -30,6 +30,8 @@ import com.mendhak.gpslogger.common.Utilities;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -37,6 +39,9 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 import arida.ufc.br.moap.core.beans.LatLonPoint;
@@ -45,7 +50,7 @@ import arida.ufc.br.moap.core.beans.Trajectory;
 import arida.ufc.br.moap.datamodelapi.imp.TrajectoryModelImpl;
 
 public class HistoryActivity extends MapActivity {
-	
+
 	private final int GMC_ZONE = -3;
 
 	public enum ResponseType {
@@ -54,29 +59,112 @@ public class HistoryActivity extends MapActivity {
 
 	// List of points provided by the remote Server in response to a request
 	private JSONArray list_of_points;
-	private final DateTimeFormatter fmt = DateTimeFormat
-			.forPattern("yyyy-M-d");
+	private DateTime start;
+	private DateTime end;
+	
+	// Format
+	private final DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-M-d");
 	private final DateTimeFormatter fmt_receive = DateTimeFormat
 			.forPattern("yyyy-M-d H:m:s");
+	
+	// TAG
 	private final String TAG = "HistoryActivity";
+	
+	 /** This integer will uniquely define the dialog to be used for displaying date picker.*/
+    private static final int DATE_START_DIALOG_ID = 0;
+    private static final int DATE_END_DIALOG_ID = 1;
+    /** Callback received when the user "picks" a <span id="IL_AD5" class="IL_AD">date in</span> the dialog */
+    private DatePickerDialog.OnDateSetListener startListener =
+            new DatePickerDialog.OnDateSetListener() {
+
+				public void onDateSet(DatePicker view, int year,
+						int monthOfYear, int dayOfMonth) {
+					try{
+						start = new DateTime(year, monthOfYear+1, dayOfMonth, 0, 0);
+						Button start_button = (Button)findViewById(R.id.start_history_button);
+						start_button.setText(start.toString(fmt));
+					}
+					catch(Exception ex){
+						Log.e(TAG,"Erro", ex);
+					}
+					
+					
+				}
+            };
+	private DatePickerDialog.OnDateSetListener endListener =
+	        new DatePickerDialog.OnDateSetListener() {
+	
+				public void onDateSet(DatePicker view, int year,
+						int monthOfYear, int dayOfMonth) {
+					try{
+					end = new DateTime(year, monthOfYear+1, dayOfMonth, 0, 0);
+					Button end_button = (Button)findViewById(R.id.end_history_button);
+					end_button.setText(end.toString(fmt));
+					}
+					catch(Exception ex){
+						Log.e(TAG,"Erro", ex);
+					}
+					
+				}
+	        };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_history);
 
-		DateTime today = new DateTime();
+		Button update_button = (Button) findViewById(R.id.update_history_button);
+		update_button.setOnClickListener(new OnClickListener() {
 
-		String username = PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getString("server_login_key", "");
-		String token = getSharedPreferences(GpsMainActivity.MOAP, Context.MODE_PRIVATE).getString("user.gpstrackerserver.token", "");
+			public void onClick(View arg0) {
+				String username = PreferenceManager
+						.getDefaultSharedPreferences(getBaseContext())
+						.getString("server_login_key", "");
+				String token = getSharedPreferences(GpsMainActivity.MOAP,
+						Context.MODE_PRIVATE).getString(
+						"user.gpstrackerserver.token", "");
+				if (username.equalsIgnoreCase("") || token.equalsIgnoreCase("")) {
+					Toast.makeText(HistoryActivity.this,
+							"You should log in to the server",
+							Toast.LENGTH_LONG).show();
+				} else {
+					if(start!=null && end != null)
+						getHistory(token, username, start, end);
+				}
+
+			}
+		});
 		
-		if(username.equalsIgnoreCase("") || token.equalsIgnoreCase("")){
-			Toast.makeText(HistoryActivity.this, "You should log in to the server", Toast.LENGTH_LONG);
-		}
-		else{
-			getHistory(token,username,today, today.plusDays(2));
-		}
+		Button start_button = (Button)findViewById(R.id.start_history_button);
+		start_button.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				showDialog(DATE_START_DIALOG_ID);
+				
+			}
+		});
+		start_button.setText("Start");
 		
+		Button end_button = (Button)findViewById(R.id.end_history_button);
+		end_button.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				showDialog(DATE_END_DIALOG_ID);
+				
+			}
+		});
+		end_button.setText("End");
+
+
+//		if (username.equalsIgnoreCase("") || token.equalsIgnoreCase("")) {
+//			Toast.makeText(HistoryActivity.this,
+//					"You should log in to the server", Toast.LENGTH_LONG);
+//		} else {
+//			getHistory(token, username, today, today.plusDays(2));
+//		}
+
 	}
 
 	@Override
@@ -89,35 +177,34 @@ public class HistoryActivity extends MapActivity {
 	@SuppressWarnings("unchecked")
 	private void onChartMenu() {
 		TrajectoryModelImpl<LatLonPoint, DateTime> model = getTrajectoryHistory();
-		
-		if(model!=null){
+
+		if (model != null) {
 			Log.w(TAG, "onCharMenu");
-			
-			System.out.println("Trajs: "+model.getTrajectoryCount());
-			
+
+			System.out.println("Trajs: " + model.getTrajectoryCount());
+
 			SpeedOverTimeChart analysis = new SpeedOverTimeChart(model);
-	
+
 			XYMultipleSeriesDataset dataset = analysis.createDataset();
 			XYMultipleSeriesRenderer renderer = analysis.createRenderer();
-	
+
 			if (dataset != null && renderer != null) {
 				try {
-					Intent intent = ChartFactory.getTimeChartIntent(this, dataset,
-							renderer, "HH:mm");
+					Intent intent = ChartFactory.getTimeChartIntent(this,
+							dataset, renderer, "HH:mm");
 					// Intent intent = ChartFactory.getLineChartIntent(this,
 					// dataset,renderer);
-	
+
 					startActivity(intent);
 				} catch (Exception ex) {
 					Utilities.LogError("ERROR CHART", ex);
 				}
-	
+
 			} else {
 				Utilities.LogDebug(String.format("Dataset - %s\nRenderer - ",
 						dataset.toString(), renderer.toString()));
 			}
-		}
-		else{
+		} else {
 			Log.w(TAG, "onCharMenu - model is null");
 		}
 	}
@@ -142,7 +229,8 @@ public class HistoryActivity extends MapActivity {
 		return false;
 	}
 
-	private void getHistory(final String token,final String username,DateTime begin, DateTime end) {
+	private void getHistory(final String token, final String username,
+			DateTime begin, DateTime end) {
 
 		new AsyncTask<DateTime, Void, ResponseType>() {
 
@@ -153,8 +241,9 @@ public class HistoryActivity extends MapActivity {
 				String url = "http://sw4.us/ufc/default.php";
 
 				// Parameters
-				url += "?PHPSESSID="+token+"&q=2" + "&id="+username + "&start=" + params[0].toString(fmt)
-						+ "&end=" + params[1].toString(fmt);
+				url += "?PHPSESSID=" + token + "&q=2" + "&id=" + username
+						+ "&start=" + params[0].toString(fmt) + "&end="
+						+ params[1].toString(fmt);
 
 				Log.d(TAG, url);
 
@@ -200,6 +289,7 @@ public class HistoryActivity extends MapActivity {
 
 							list_of_points = jsonObject.getJSONObject("result")
 									.getJSONArray("point");
+							Log.i(TAG, "Number of points "+list_of_points.length());
 						} else {
 
 							Log.w("HistoryActivity",
@@ -239,11 +329,14 @@ public class HistoryActivity extends MapActivity {
 				// Check the result of the process
 				switch (v) {
 				case OK:
+					Toast.makeText(HistoryActivity.this,
+							"Loaded "+list_of_points.length()+" points",
+							Toast.LENGTH_LONG).show();
 					drawTrajectoryHistory();
 					break;
 				case INTERNET_ISSUE:
 					Toast.makeText(getApplicationContext(),
-							"Cannot connect to the Server", Toast.LENGTH_SHORT)
+							"Cannot connect to the Server", Toast.LENGTH_LONG)
 							.show();
 					break;
 				case ERROR:
@@ -258,11 +351,11 @@ public class HistoryActivity extends MapActivity {
 
 	}
 
-	@SuppressWarnings({ "unchecked"})
+	@SuppressWarnings({ "unchecked", "unused" })
 	private TrajectoryModelImpl<LatLonPoint, DateTime> getTrajectoryHistory() {
-		
+
 		Log.d(TAG, "get trajectory history");
-		
+
 		TrajectoryModelImpl<LatLonPoint, DateTime> model = null;
 		if (this.list_of_points != null) {
 			int size = this.list_of_points.length();
@@ -275,19 +368,22 @@ public class HistoryActivity extends MapActivity {
 			Trajectory<LatLonPoint, DateTime> traj = model.factory()
 					.newTrajectory(mo.getId() + "_0", mo);
 			try {
-				Log.d(TAG, " GetTrajectoryHistory - Loading trajectory model from JSON");
+				Log.d(TAG,
+						" GetTrajectoryHistory - Loading trajectory model from JSON");
 				for (int i = 0; i < size; i++) {
 					JSONObject object = this.list_of_points.getJSONObject(i);
 					double lat = object.getDouble("lat");
 					double lon = object.getDouble("long");
 					LatLonPoint point = new LatLonPoint(lon, lat);
 					String date = object.getString("time");
-					
+
 					DateTime datetime;
-					if(GMC_ZONE < 0)
-						datetime = fmt_receive.parseDateTime(date).minusHours(GMC_ZONE*-1);
-					else{
-						datetime = fmt_receive.parseDateTime(date).plusHours(GMC_ZONE);
+					if (GMC_ZONE < 0)
+						datetime = fmt_receive.parseDateTime(date).minusHours(
+								GMC_ZONE * -1);
+					else {
+						datetime = fmt_receive.parseDateTime(date).plusHours(
+								GMC_ZONE);
 					}
 					traj.addPoint(point, datetime);
 
@@ -343,16 +439,16 @@ public class HistoryActivity extends MapActivity {
 				}
 
 				overlayList.add(overlay);
-				
+
 				// Set zoom
-				JSONObject object = this.list_of_points.getJSONObject(size-1);
+				JSONObject object = this.list_of_points.getJSONObject(size - 1);
 				double lat = object.getDouble("lat");
 				double lon = object.getDouble("long");
 
 				GeoPoint geoPoint = new GeoPoint(
 						Utilities.convertCoordinates(lat),
 						Utilities.convertCoordinates(lon));
-				
+
 				mapView.getController().setCenter(geoPoint);
 			} catch (Exception e) {
 
@@ -363,5 +459,25 @@ public class HistoryActivity extends MapActivity {
 		}
 
 	}
+	@Override
+    protected Dialog onCreateDialog(int id) {
+		DateTime now = new DateTime();
+		DatePickerDialog pd;
+        switch (id) {
+        case DATE_START_DIALOG_ID:
+        	pd = new DatePickerDialog(this, 
+                    startListener,
+                    now.getYear(), now.getMonthOfYear()-1, now.getDayOfMonth());
+        	pd.setTitle("Start Date");
+            return pd;
+        case DATE_END_DIALOG_ID:
+        	pd = new DatePickerDialog(this, 
+                    endListener,
+                    now.getYear(), now.getMonthOfYear()-1, now.getDayOfMonth());
+        	pd.setTitle("End Date");
+                return pd;
+        }
+        return null;
+    }
 
 }
